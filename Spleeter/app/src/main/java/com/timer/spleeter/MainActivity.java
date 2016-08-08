4a3bf92da25fe71cf9bf71d56a0b4728f18f1d3d
Pierre GRASSER <piorrro33@outlook.com>
2016-08-08 23:30:11 +0200

    Added an action bar.

--- Spleeter/app/src/main/java/com/timer/spleeter/MainActivity.java
+++ Spleeter/app/src/main/java/com/timer/spleeter/MainActivity.java
@@ -2,13 +2,21 @@ package com.timer.spleeter;
 
 import android.app.*;
 import android.os.*;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends Activity 
+
+public class MainActivity extends ActionBarActivity 
 {
+	private Toolbar mToolbar;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
+		mToolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(mToolbar);
     }
 }
