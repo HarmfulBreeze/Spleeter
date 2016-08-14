2321bcf3cf860bec28decdaa22c8efc54391a963
Piorrro33 <piorrro33@outlook.com>
2016-08-14 23:58:31 +0200

    Fixed a wrong resource name (I'm retarded)

--- app/src/main/java/com/timer/spleeter/MainActivity.java
+++ app/src/main/java/com/timer/spleeter/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
 		
 		mToolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(mToolbar);
